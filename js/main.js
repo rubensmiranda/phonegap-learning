cda6dca38106e1ee1d14fd2cbce1c4e49fda8aca
rlobato_cit <rlobato@ciandt.com>
2015-09-08 16:50:15 -0300

    teste

--- js/main.js
+++ js/main.js
@@ -1,23 +1,18 @@
-var app = {
+var app = angular.module('minmax', []);
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
 
-    initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+app.controller('MinMaxCtrl', function ($scope, $http) {
+	$scope.formModel = {};
 
-};
+	$scope.onSubmit = function () {
+		console.log("Hey i'm submitted!");
+		console.log($scope.formModel);
 
-app.initialize();
+		$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
+			success(function (data) {
+				console.log(":)")
+			}).error(function(data) {
+				console.log(":(")
+			});
+	};
+});
